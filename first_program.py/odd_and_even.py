c00c7de702f82562fe7644e817606d247698c31f
ivanmula-1 <ivanmula74@gmail.com>
2026-04-29 14:45:38 +0800

    Add DataStreamer class for number collection

--- first_program.py/odd_and_even.py
+++ first_program.py/odd_and_even.py
@@ -6,3 +6,8 @@ class ANSIFormatter:
     EMERALD = '\033[92m'
     END = '\033[0m'
     BOLD = '\033[1m'
+class DataStreamer:
+    def __init__(self, target_file="numbers.txt"):
+        self.target_file = target_file
+        self.even_collection = []
+        self.odd_collection = []
